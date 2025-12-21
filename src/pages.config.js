e13309950b917f827c4ec7caaa845a8d6adfdae5
Aviel Cohen <coaviel@gmail.com>
2025-12-21 15:53:44 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,25 +1,25 @@
 import Customers from './pages/Customers';
-import Dashboard from './pages/Dashboard';
 import NewShipment from './pages/NewShipment';
 import Profile from './pages/Profile';
 import ReportView from './pages/ReportView';
-import Reports from './pages/Reports';
 import ShipmentView from './pages/ShipmentView';
 import Shipments from './pages/Shipments';
 import Support from './pages/Support';
+import Dashboard from './pages/Dashboard';
+import Reports from './pages/Reports';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Customers": Customers,
-    "Dashboard": Dashboard,
     "NewShipment": NewShipment,
     "Profile": Profile,
     "ReportView": ReportView,
-    "Reports": Reports,
     "ShipmentView": ShipmentView,
     "Shipments": Shipments,
     "Support": Support,
+    "Dashboard": Dashboard,
+    "Reports": Reports,
 }
 
 export const pagesConfig = {
