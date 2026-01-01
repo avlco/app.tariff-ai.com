a65050c3ecfaa3212fcd2b8aacbebbc97af26229
Aviel Cohen <aviel.cohen@gramss.app>
2026-01-01 18:47:45 +0200

    Remove Toaster component from App.jsx
    Removed Toaster component from App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import VisualEditAgent from '@/lib/VisualEditAgent'
@@ -74,7 +73,6 @@ function App() {
           <NavigationTracker />
           <AuthenticatedApp />
         </Router>
-        <Toaster />
         <VisualEditAgent />
       </QueryClientProvider>
     </AuthProvider>
